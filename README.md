9ddea04a5d65f0c704ea24580544c6fd16485595
Cédric <cedced19@gmail.com>
2016-02-11 22:07:18 +0100

    Add check-update-github

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 A static software to host your lessons.
 Here it is the generator of this software.
 
+[Preview](https://cedced19.github.io/learn-memory-static/)
+
 [![Build Status](https://travis-ci.org/cedced19/learn-memory-static.svg)](https://travis-ci.org/cedced19/learn-memory-static)
 
 ## Installation
@@ -10,6 +12,7 @@ __Required:__ Node.js and git
 
 ```bash
 $ git clone --depth=1 --branch=master https://github.com/cedced19/zikcenter-static
+$ npm install --production
 $ node data-generator.js -f data.json
 ```
 
--- data-generator.js
+++ data-generator.js
@@ -7,12 +7,11 @@ var fs = require('fs'),
    pkg = require('./package.json'),
    isThere =  require('is-there');
 
-var matcher = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/; // check whether a string is a URL
-
 program
   .version(pkg.version)
   .option('-s, --server  [string]', 'specified the adress where the generator can get the data "example.com:7772"')
   .option('-f, --filename  [string]', 'specified the data file where the generator can get the data "data.json"')
+  .option('-c, --check', 'check if there is an update')
   .parse(process.argv);
 
 var writeStream = fs.createWriteStream('dist/data.json');
@@ -27,6 +26,7 @@ writeStream.on('finish', function () {
 
 if (program.server) {
   var url = 'http://' + program.server;
+  var matcher = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/; // check whether a string is a URL
   if (matcher.test(url)) {
     request(url + '/api/long/')
     .on('error', function(err) {
@@ -49,4 +49,17 @@ if (program.server) {
     });
 } else {
   console.log('Please set a server adress or a filename.'.red);
+}
+
+if (program.check) {
+    require('check-update-github')({
+      name: pkg.name,
+      currentVersion: pkg.version,
+      user: 'cedced19',
+      branch: 'master'
+    }, function(err, latestVersion, defaultMessage){
+        if(!err){
+            console.log(defaultMessage);
+        }
+    });
 }
